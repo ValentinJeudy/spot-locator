8f93dad9b2c2c55bb49b19f8a397bb2f00f7c27b
valentin.jeudy.smile <vjeudy@amiltone.fr>
2017-09-30 20:29:32 +0200

    adding new components

---
+++ src/components/Gmap.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="gmap">
+    <b-container-fluid>
+      <div class="sidebar">
+        <h2>Barre latérale</h2>
+      </div>
+      <div class="map">
+
+      </div>
+    </b-container-fluid>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    createMap () {
+      // const mapElement = this.$el.querySelector('.map')
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang=scss>
+  @import "../assets/scss/gmap.scss";
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="login">
+    <h1>login page</h1>
+    <b-container>
+      <b-row align-v="center" align-h="center">
+          <b-col cols="5">
+            <h2>Connectez-vous !</h2>
+            <form action="">
+              <label for="">mail :</label><br>
+              <input type="text"><br>
+              <label for="">Password :</label><br>
+              <input type="text">
+            </form>
+          </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang=scss>
+  @import "../assets/scss/login.scss";
+
+</style>
